03b82844a9dbac9b362d8e29643ae8d553b71eaa
wannabemountainking <kdy0096@gmail.com>
2024-11-06 20:26:34 +0900

    berlinscrapper class

--- assignment_9.py
+++ assignment_9.py
@@ -15,77 +15,118 @@ import re
 from pprint import pprint
 
 
-URL = "https://berlinstartupjobs.com"
-jobs_db = []
+class BerlinStartupScrapper:
 
-def activate_page(url):
-	p = sync_playwright().start()
-	browser = p.chromium.launch(headless=False)
-	page = browser.new_page()
-	page.goto(url)
-	content = page.content()
-	soup = BeautifulSoup(content, 'html.parser')
-	p.stop()
-	return (page, soup)
-
-def get_info_of_engineering_jobs():
-	active_page_soup = activate_page(f"{URL}/engineering/")
-	page = active_page_soup[0]
-	soup = active_page_soup[1]
-	engineering_jobs = get_skill_db(page, soup)
-	return engineering_jobs
-
-def collecting_skill_areas_jobs_db(url):
-	active_page_soup = activate_page(f"{URL}/engineering/")
-	# page = active_page_soup[0]
-	soup = active_page_soup[1]
-	skill_sets = get_skillset_dict(soup)
-	for (skill_area, link) in skill_sets.items():
-		job_db = get_skill_db(link)
-		job_db_dict = {skill_area: job_db}
-		jobs_db.append(job_db_dict)
-	return jobs_db
+	def __init__(self):
+		self.url = "https://berlinstartupjobs.com"
+		self.jobs_db = []
 
 
-def get_skillset_dict(soup):
-	skill_dict = {}
-	popular_skills = soup.find_all("a", class_="bjs-bl--dolphin")
-	for skill in popular_skills:
-		skill_name = re.sub("\n|\t", "", skill.text.split(" ")[0])
-		skill_dict[skill_name] = skill['href']
-	return skill_dict
+	def total_jobs(self):
 
-def get_skill_db(url):
-	jobs_according_to_skills = activate_page(url)
-	page = jobs_according_to_skills[0]
-	soup = jobs_according_to_skills[1]
-	job_db = []
-	job_pages = soup.find_all(class_="page-numbers")
-	number_of_ages = 0
-	if job_pages:
-		number_of_pages = len(job_pages)
-	else:
-		number_of_pages = 2
+		# 전체 구직 정보를 리턴함
+		# 모든 결과는 이 함수를 통해 확보
 
-	for i in range(1, number_of_pages):
-		if i != 1:
-			page.goto(f"{URL}/engineering/page/{i}/")
-			newContent = page.content()
-			soup = BeautifulSoup(newContent, 'html.parser')
-		jobs = soup.find_all("li", class_="bjs-jlid")
-		for job in jobs:
-			first_link = job.find("a")
-			title = first_link.text
-			company = job.find("a", class_="bjs-jlid__b").text
-			job_description = re.sub("\n|\t", "", job.find("div", class_="bjs-jlid__description").text)
-			link = first_link["href"]
-			job_data = dict(
-				company=company,
-				job_title=title,
-				job_description=job_description,
-				link=link
-			)
-			job_db.append(job_data)
-		return job_db
+		engineering = dict(engineering=self.get_info_of_engineering_jobs())
+		skill_areas_jobs = self.collecting_skill_areas_jobs_db()
+		skill_areas_jobs.append(engineering)
+		return skill_areas_jobs
 
-pprint(collecting_skill_areas_jobs_db(URL))
+	def get_info_of_engineering_jobs(self):
+
+		# engineering 분야의 구직 정보를 리턴한다.
+		# return : dict => {'company': 회사명, ...}
+		engineering_jobs = self.get_skill_db(f"{self.url}/engineering")
+		return engineering_jobs
+
+	def collecting_skill_areas_jobs_db(self):
+
+		# skill_areas의 구직정보 데이터를 skill별로 모아 리턴
+		# parameter: url
+		# return: [{skill_area 명: [{구직정보}, ...]}, ...]
+
+		active_page_soup = self.activate_page(f"{self.url}/engineering/")
+		soup = active_page_soup[1]
+		skill_sets = self.get_skillset_dict(soup)
+		for (skill_area, link) in skill_sets.items():
+			job_db = self.get_skill_db(link)
+			job_db_dict = {skill_area: job_db}
+			self.jobs_db.append(job_db_dict)
+		return self.jobs_db
+
+	def get_skillset_dict(self, soup):
+
+		# skilㅣ에 따른 구직 정보 페이지(링크)를 dictionary 형태로 리턴한다.
+		# parameter: BeautifulSoup 객체
+		# return: dictionary => {'skill_name': 'skill_area/기술명' 링크}
+
+		skill_dict = {}
+		popular_skills = soup.find_all("a", class_="bjs-bl--dolphin")
+		for skill in popular_skills:
+			skill_name = re.sub("\n|\t", "", skill.text.split(" ")[0])
+			skill_dict[skill_name] = skill['href']
+		return skill_dict
+
+	def get_skill_db(self, url):
+
+		# 각 skill_area 의 정보를 모아서 딕셔너리로 리턴한다.
+		# parameter: url
+		# return: {
+		# 				'company': company 명,
+		# 				'job_title': 구직 제목,
+		# 				'job_description': job_description,
+		# 				'link': link
+		# 				}
+
+		jobs_according_to_skills = self.activate_page(url)
+		page = jobs_according_to_skills[0]
+		soup = jobs_according_to_skills[1]
+		job_db = []
+		job_pages = soup.find_all(class_="page-numbers")
+		number_of_pages = 0
+		if job_pages:
+			number_of_pages = len(job_pages)
+		else:
+			number_of_pages = 2
+
+		for i in range(1, number_of_pages):
+			if i != 1:
+				page.goto(f"{self.url}/engineering/page/{i}/")
+				newContent = page.content()
+				soup = BeautifulSoup(newContent, 'html.parser')
+			jobs = soup.find_all("li", class_="bjs-jlid")
+			for job in jobs:
+				first_link = job.find("a")
+				title = first_link.text
+				company = job.find("a", class_="bjs-jlid__b").text
+				job_description = re.sub("\n|\t", "", job.find("div", class_="bjs-jlid__description").text)
+				link = first_link["href"]
+				job_data = dict(
+					company=company,
+					job_title=title,
+					job_description=job_description,
+					link=link
+				)
+				job_db.append(job_data)
+			return job_db
+
+
+	def activate_page(self, url):
+
+		# url을 입력받아 playwright의 page 객체, BeautifulSoup 객체를 튜플형태로 리턴한다.
+		# parameter: url 기본값 berlin구직 사이트 메인 홈
+		# return: 튜플 => (page, soup)
+
+
+		p = sync_playwright().start()
+		browser = p.chromium.launch(headless=False)
+		page = browser.new_page()
+		page.goto(url)
+		content = page.content()
+		soup = BeautifulSoup(content, 'html.parser')
+		p.stop()
+		return (page, soup)
+
+scrapper = BerlinStartupScrapper()
+result = scrapper.total_jobs()
+pprint(result)
